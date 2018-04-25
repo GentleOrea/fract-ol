9cfeeac29dca6bc868dd0ad3e8c83889de72ff85
Yoan GARROT <ygarrot@e3r8p8.42.fr>
2018-04-25 19:31:51 +0200

    Add some visu

--- src/init.c
+++ src/init.c
@@ -6,7 +6,7 @@
 /*   By: ygarrot <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/03/06 11:31:40 by ygarrot           #+#    #+#             */
-/*   Updated: 2018/03/16 10:40:06 by ygarrot          ###   ########.fr       */
+/*   Updated: 2018/04/25 19:27:21 by ygarrot          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,8 +21,8 @@ void	init_images(t_global *g, int len, int height)
 
 void	init_global(t_global *g)
 {
-	g->win.max_len = 600;
-	g->win.max_height = 600;
+	g->win.max_len = 700;
+	g->win.max_height = 700;
 	g->win.mlx_ptr = mlx_init();
 	g->win.win_ptr = mlx_new_window(g->win.mlx_ptr,
 			g->win.max_len, g->win.max_height, "fractol");
